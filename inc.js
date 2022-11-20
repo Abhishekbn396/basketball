679360c3deff3c070918cc297a73ef9c63e255a4
Abhishekbn396 <98246994+Abhishekbn396@users.noreply.github.com>
2022-11-20 20:07:41 +0530

    Add files via upload

--- inc.js
+++ inc.js
@@ -4,52 +4,44 @@ let hcount=0
 let gcount=0
 let h=hpoint.innerText
 let g=gpoint.innerText
-function changecolor(h,g){
-    if (g< h) {
-        let h= '<p style="color:green;">' + h+ '</p>'
-    }else if (g>= h) {
-        let g= '<p style="color:green;">' + g + '</p>'
-    }
-}
+// function changecolor(h,g){
+//     if (g< h) {
+//         let h= '<p style="color:green;">' + h+ '</p>'
+//     }else if (g>= h) {
+//         let g= '<p style="color:green;">' + g + '</p>'
+//     }
+// }
 function homeincby1(){
     hcount+=1
     hpoint.innerText=hcount
-    changecolor(h,g)
 }
 function homeincby2(){
     hcount+=2
     hpoint.innerText=hcount
-    changecolor(h,g)
 }
 function homeincby3(){
     hcount+=3
     hpoint.innerText=hcount
-    changecolor(h,g)
 }
 function homeincby4(){
     hcount-=1
     hpoint.innerText=hcount
-    changecolor(h,g)
 }
 function guestincby1(){
     gcount+=1
     gpoint.innerText=gcount
-    changecolor(h,g)
 }
 function guestincby2(){
     gcount+=2
     gpoint.innerText=gcount
-    changecolor(h,g)
 }
 function guestincby3(){
     gcount+=3
     gpoint.innerText=gcount
-    changecolor(h,g)
 }
 function guestincby4(){
     gcount-=1
     gpoint.innerText=gcount
-    changecolor(h,g)
 }
 function resetgame(){
     hpoint.innerText=0
